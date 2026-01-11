a5c832f272c971ef2bdfd4535e516330d8ed46f7
Kaosar Ali <kaosar3662@gmail.com>
2026-01-11 17:18:22 +0600

    Dont Need Details Page For Now

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Route::group(['prefix' => 'inventory', 'as' => 'products.'], function () {
     Route::post('/store', [ProductController::class, 'store'])->name('store');
 
     // Show/Edit/Delete using SLUG instead of ID
-    Route::get('/{product:slug}', [ProductController::class, 'show'])->name('show');
+    // Route::get('/{product:slug}', [ProductController::class, 'show'])->name('show');
     Route::get('/{product:slug}/edit', [ProductController::class, 'edit'])->name('edit');
     Route::put('/{product:slug}/update', [ProductController::class, 'update'])->name('update');
     Route::delete('/{product:slug}/delete', [ProductController::class, 'destroy'])->name('destroy');
